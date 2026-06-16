3b5c4e7521039528ce2472194ebd692a5969221d
Rutvik2302 <rutviktank23@gmail.com>
2026-06-16 12:07:43 +0530

    Email Send by render done

--- helper/sendVerificationemail.ts
+++ helper/sendVerificationemail.ts
@@ -1,2 +1,20 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "@/emails/VerificationEmail";
+import { ApiResponse } from "@/types/apiResponse";
+import { promises } from "node:dns";
 
+
+export async function SendVerificationEmail(email: string, username: string, verifycode: string): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from?: 'onboarding@resend.dev',
+            to:email,
+            subject:'Varification Code Myapp',
+            react:VerificationEmail({username , otp:verifycode}),
+        });
+        return { success: true, message: "send vrification email to register mail" }
+    } catch (error) {
+        console.log(error, "Sending the email id faild")
+        return { success: false, message: "Failld to send vrification email" }
+    }
+}
